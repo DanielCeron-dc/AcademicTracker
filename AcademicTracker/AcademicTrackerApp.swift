a0d660a4ca1ad4e1949dd0778b7fa926e3a70f66
danieceron <danieceron.itoeste@gmail.com>
2026-04-18 18:23:04 -0500

    Initial Commit

---
+++ AcademicTracker/AcademicTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcademicTrackerApp.swift
+//  AcademicTracker
+//
+//  Created by Daniel Ceron on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct AcademicTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AcademicTrackerTests/AcademicTrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  AcademicTrackerTests.swift
+//  AcademicTrackerTests
+//
+//  Created by Daniel Ceron on 4/18/26.
+//
+
+import Testing
+@testable import AcademicTracker
+
+struct AcademicTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
